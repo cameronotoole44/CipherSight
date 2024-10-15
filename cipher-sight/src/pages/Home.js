51895174c47130f90e812398db0a69d0c014edaa
cameronotoole44 <cameronotoole404@gmail.com>
2024-10-15 11:25:15 -0400

    home starter

---
+++ cipher-sight/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to Cipher Sight!</h1>
+            <p>A Visual representation for algorithms and data types</p>
+        </div>
+    );
+}
+
+export default Home;
