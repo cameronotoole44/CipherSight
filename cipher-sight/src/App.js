65d556f1f39c4add2cbed264c62401b39a6bc87a
cameronotoole44 <cameronotoole404@gmail.com>
2024-10-15 11:57:52 -0400

    proper route

--- cipher-sight/src/App.js
+++ cipher-sight/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import ErrorPage from './components/ErrorPage';
+import ErrorPage from './pages/Error';
 import Home from './pages/Home';
 import Algorithm from './pages/Algorithm';
 
