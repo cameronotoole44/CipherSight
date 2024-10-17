e52513c5a97fa7773743ef3a6455c580d6718fe1
cameronotoole44 <cameronotoole404@gmail.com>
2024-10-17 19:49:47 -0400

    added search algorithms to the drop down

--- cipher-sight/src/components/Controls.js
+++ cipher-sight/src/components/Controls.js
@@ -15,8 +15,14 @@ const Controls = ({ onStart, onPause, onReset, onAlgorithmChange }) => {
                 <option value="quickSort">Quick Sort</option>
                 <option value="mergeSort">Merge Sort</option>
                 <option value="selectionSort">Selection Sort</option>
+                <option value="insertionSort">Insertion Sort</option>
 
+                <option value="linearSearch">Linear Search</option>
+                <option value="binarySearch">Binary Search</option>
+                <option value="dfs">Depth-First Search (DFS)</option>
+                <option value="bfs">Breadth-First Search (BFS)</option>
             </select>
+
             <button onClick={onStart} className='control-btn'>Start</button>
             <button onClick={onPause} className='control-btn'>Pause</button>
             <button onClick={onReset} className='control-btn'>Reset</button>
