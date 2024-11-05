f59131c205ae8a1ba5e29368c07b50abdda3ea5b
cameronotoole44 <cameronotoole404@gmail.com>
2024-11-04 22:30:54 -0500

    functioning linear search

--- cipher-sight/src/algorithms/search/linearSearch.js
+++ cipher-sight/src/algorithms/search/linearSearch.js
@@ -1,17 +1,34 @@
-const linearSearch = async (array, updateVisualizer, animationSpeed, target) => {
-    for (let i = 0; i < array.length; i++) {
-        updateVisualizer({
+const linearSearch = async (arr, updateVisualizer, delay, target) => {
+    for (let i = 0; i < arr.length; i++) {
+        // highlight element thats being checked
+        await updateVisualizer({
+            type: 'search',
             currentIndex: i,
-            found: array[i] === target,
-            visitedIndices: Array.from({ length: i + 1 }, (_, idx) => idx)
+            visitedIndices: Array.from({ length: i }, (_, idx) => idx),
+            found: arr[i] === target
         });
 
-        await new Promise(resolve => setTimeout(resolve, animationSpeed));
+        await new Promise(resolve => setTimeout(resolve, delay));
 
-        if (array[i] === target) {
+        if (arr[i] === target) {
+            // target found
+            await updateVisualizer({
+                type: 'search',
+                currentIndex: i,
+                visitedIndices: Array.from({ length: i + 1 }, (_, idx) => idx),
+                found: true
+            });
             return i;
         }
     }
+
+    // target not found
+    await updateVisualizer({
+        type: 'search',
+        currentIndex: -1,
+        visitedIndices: Array.from({ length: arr.length }, (_, idx) => idx),
+        found: false
+    });
     return -1;
 };
 
